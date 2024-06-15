3e427ac72226d4a8641289414bf9743e67bdc685
Cameron Leverett <cameron@ntx.ventures>
2024-06-14 18:08:08 -0700

    updated, data helpers, formatDate to except string | Date

--- src/components/layouts/main/helpers/date.ts
+++ src/components/layouts/main/helpers/date.ts
@@ -1,10 +1,10 @@
 export function formatDate(
-  date: string,
+  date: string | Date,
   options: Intl.DateTimeFormatOptions = {
     month: "short",
     day: "numeric",
     year: "numeric",
   }
 ) {
-  return new Intl.DateTimeFormat("en-US", options).format(new Date(date));
+  return new Intl.DateTimeFormat("en-US", options).format(typeof date == "string" ? new Date(date) : date);
 }
